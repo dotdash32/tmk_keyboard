437da17ee820d9802ff98c34435862963a1ab346
dotdash32 <dotdash32@gmail.com>
2015-11-11 14:23:03 -0800

    Product name

--- keyboard/split_keyboard/config.h
+++ keyboard/split_keyboard/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x4444
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    tmk-split
-#define PRODUCT         tmk-split-keyboard
+#define PRODUCT         Thirty-Eight-Pecenter
 #define DESCRIPTION     tmk split keyboard firmware
 
 /* key matrix size */
